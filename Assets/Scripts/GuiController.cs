afc1c8e7d57d55a05ec58662f48151d9412a9759
Jake Stanley <prudentia713@gmail.com>
2016-01-11 22:53:28 +0000

    added resource replenishment

--- Assets/Scripts/GuiController.cs
+++ Assets/Scripts/GuiController.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class GuiController : MonoBehaviour {
 
     public ShipController ship;
+    public LandingPadController landingPad;
     private int width, height;
 
     // shapes
-    private Rect infoBox;
+    private Rect shipInfoBox, padInfoBox;
 
     // Use this for initialization
     void Start () {
@@ -40,28 +41,51 @@ public class GuiController : MonoBehaviour {
 
     private void buildInfoBox(){
 
-        // box
+        // boxes
         int infoBoxHeight = (VERTICAL_MARGIN * 4) + (SINGLE_LINE_HEIGHT * 3);
-        infoBox = new Rect(HORIZONTAL_MARGIN, VERTICAL_MARGIN, INFO_BOX_WIDTH, infoBoxHeight);
+
+        shipInfoBox = new Rect( HORIZONTAL_MARGIN, 
+                                VERTICAL_MARGIN, 
+                                INFO_BOX_WIDTH, 
+                                infoBoxHeight);
+
+        padInfoBox  = new Rect( width - HORIZONTAL_MARGIN - INFO_BOX_WIDTH, 
+                                height - infoBoxHeight - VERTICAL_MARGIN, 
+                                INFO_BOX_WIDTH, 
+                                infoBoxHeight);
         
-
         // labels
-        float x = infoBox.x + HORIZONTAL_MARGIN;
+        float x = shipInfoBox.x + HORIZONTAL_MARGIN;
         float labelWidth = INFO_BOX_WIDTH - (HORIZONTAL_MARGIN*2);
+        float padLabelX = padInfoBox.x + HORIZONTAL_MARGIN;
 
-        // create rect
-        Rect topLabelRect       = new Rect(x, infoBox.y + VERTICAL_MARGIN*2,         labelWidth, SINGLE_LINE_HEIGHT);
+        // create rects
+        Rect topLabelRect       = new Rect(x, shipInfoBox.y + VERTICAL_MARGIN*2,     labelWidth, SINGLE_LINE_HEIGHT);
         Rect middleLabelRect    = new Rect(x, topLabelRect.y + VERTICAL_MARGIN*2,    labelWidth, SINGLE_LINE_HEIGHT);
         Rect bottomLabelRect    = new Rect(x, middleLabelRect.y + VERTICAL_MARGIN*2, labelWidth, SINGLE_LINE_HEIGHT);
 
+        Rect padInfoTopLabelRect       = new Rect(padLabelX, this.height - infoBoxHeight + VERTICAL_MARGIN*3,             labelWidth, SINGLE_LINE_HEIGHT);
+        Rect padInfoMiddleLabelRect    = new Rect(padLabelX, padInfoTopLabelRect.y + VERTICAL_MARGIN*2,    labelWidth, SINGLE_LINE_HEIGHT);
+        Rect padInfoBottomLabelRect    = new Rect(padLabelX, padInfoMiddleLabelRect.y + VERTICAL_MARGIN*2, labelWidth, SINGLE_LINE_HEIGHT);
+
         // update info box y max
-        infoBox.yMax = bottomLabelRect.yMax + VERTICAL_MARGIN;
+        shipInfoBox.yMax = bottomLabelRect.yMax + VERTICAL_MARGIN;
+        padInfoBox.yMax = padInfoBottomLabelRect.yMax - VERTICAL_MARGIN;
+        padInfoBox.height = padInfoBox.yMax - padInfoBox.y;
+        padInfoBox.y += VERTICAL_MARGIN*2;
 
         // create the GUIs
-        GUI.Box(infoBox, "Parameters");
+        GUI.Box(shipInfoBox, "Ship supplies");
         GUI.Label(topLabelRect,     "Fuel: "        + ship.fuel);
         GUI.Label(middleLabelRect,  "Battery: "     + ship.battery);
         GUI.Label(bottomLabelRect,  "Integrity: "   + ship.hull);
+
+        if(landingPad.isAttached){
+            GUI.Box(padInfoBox, "Base supplies");
+            GUI.Label(padInfoTopLabelRect,      "Fuel supplies: "       + landingPad.fuelSupplies);
+            GUI.Label(padInfoMiddleLabelRect,   "Battery supplies: "    + landingPad.batterySupplies);
+            GUI.Label(padInfoBottomLabelRect,   "Repair supplies: "     + landingPad.repairSupplies);
+        }
 
     }
 
---
+++ Assets/Scripts/LandingPadController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandingPadController : MonoBehaviour {
+
+    public float fuelSupplies, batterySupplies, repairSupplies;
+
+    public Collider shipCollider;
+    public Collider landingPadCollider;
+    public ShipController shipController;
+    public bool isAttached;
+
+
+	// Use this for initialization
+	void Start () {
+        isAttached = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // replenish the ship
+        if(isAttached){
+
+            // get required amounts
+            float fuelAmountRequired        = ShipController.MAX_FUEL       - shipController.fuel;
+            float batteryRechargeRequired   = ShipController.MAX_BATTERY    - shipController.battery;
+            float repairAmountRequired      = ShipController.MAX_HULL       - shipController.hull;
+
+            // TODO make sure you only recharge/refuel/repair/whatever the amount we have. don't charge 10 if we've only got 5 supplies
+
+            float fuelAmountExpended            = 0;
+            float batteryRechargeExpended       = 0;
+            float repairSuppliesExpended        = 0;
+
+            if(fuelAmountRequired >= REFUEL_RATE){
+                fuelAmountExpended = REFUEL_RATE;
+            } else {
+                fuelAmountExpended = fuelAmountRequired;
+            }
+
+            if(batteryRechargeRequired >= RECHARGE_RATE){
+                batteryRechargeExpended = RECHARGE_RATE;
+            } else {
+                batteryRechargeExpended = batteryRechargeRequired;
+            }
+
+            if(repairAmountRequired >= REPAIR_RATE){
+                repairSuppliesExpended = REPAIR_RATE;
+            } else {
+                repairSuppliesExpended = repairAmountRequired;
+            }
+
+            if(fuelAmountExpended > fuelSupplies){
+                fuelAmountExpended = fuelSupplies;
+            }
+
+            if(batteryRechargeExpended > batterySupplies){
+                batteryRechargeExpended = batterySupplies;
+            }
+
+            if(repairSuppliesExpended > repairSupplies){
+                repairSuppliesExpended = repairSupplies;
+            }
+
+            shipController.fuel += fuelAmountExpended;
+            shipController.battery += batteryRechargeExpended;
+            shipController.hull += repairSuppliesExpended;
+
+            fuelSupplies    -= fuelAmountExpended;
+            batterySupplies -= batteryRechargeExpended;
+            repairSupplies  -= repairSuppliesExpended;
+
+        }
+
+        // TODO stop the relevant animations if at max (or if we've run out)
+        // TODO restart the animations if the levels get back up
+
+    }
+
+
+    // collider
+    void OnCollisionEnter(Collision collision){
+
+        // TODO start the relevant animations
+
+        Debug.Log("Collision enter");
+        isAttached = true;
+    }
+
+    void OnCollisionExit(Collision collision){
+        Debug.Log("Collision leave");
+        // if(col == shipCollider){
+            // Debug.Log("Landed");
+        // }
+        isAttached = false;
+    }
+
+    private const float REFUEL_RATE = 0.2f;
+    private const float RECHARGE_RATE = 0.1f;
+    private const float REPAIR_RATE = 0.05f;
+
+
+
+}
