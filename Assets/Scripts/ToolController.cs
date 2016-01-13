3d069ea54887a865db691607103d4f4ac8f258e9
Jake Stanley <prudentia713@gmail.com>
2016-01-13 22:33:59 +0000

    got tool controls working, added some factory basics

--- Assets/Scripts/ToolController.cs
+++ Assets/Scripts/ToolController.cs
@@ -6,80 +6,85 @@ public class ToolController : MonoBehaviour {
     public int tool;
     public int toolStatus;
     public bool toolLocked;
-    public float drillInitialOffsetY, drillOffsetY, clawInitialOffsetY, clawOffsetY, maxFuckingOffsetY, minFuckingOffsetY;
+    public float toolOffsetY;
     public Transform drill, claw;
 
 	// Use this for initialization
 	void Start () {
 
-        drillOffsetY = 0;
-        clawOffsetY = 0;
-
+        toolOffsetY = 0;
         tool = TOOL_DRILL;
         toolStatus = TOOL_RETRACTED;
         toolLocked = false;
-
-        maxFuckingOffsetY  = drill.transform.position.y;
-        minFuckingOffsetY  = maxFuckingOffsetY - MAX_OFFSET_Y_DRILL;
 
     }
     
     // Update is called once per frame
     void Update () {
 
-        drillInitialOffsetY  = drill.transform.position.y;
-        clawInitialOffsetY   = claw.transform.position.y;
+        if(!toolLocked){
+            if(Input.GetKey(KeyCode.Alpha1)){
+                toolStatus = TOOL_SWITCHING_TO_DRILL;
+            } else if(Input.GetKey(KeyCode.Alpha2)){
+                toolStatus = TOOL_SWITCHING_TO_CLAW;
+            }
 
-        if(Input.GetKey(KeyCode.LeftShift) && !toolLocked){
-            toolStatus = TOOL_DEPLOYED;
-        } else {
-            toolStatus = TOOL_RETRACTED;
+            if(Input.GetKey(KeyCode.LeftShift) && (toolStatus != TOOL_SWITCHING_TO_DRILL) && (toolStatus != TOOL_SWITCHING_TO_CLAW)){
+                toolStatus = TOOL_DEPLOYED;
+            } else if((toolStatus != TOOL_SWITCHING_TO_DRILL) && (toolStatus != TOOL_SWITCHING_TO_CLAW)){
+                toolStatus = TOOL_RETRACTED;
+            }
+
         }
 
-        float increaseAmount = Time.deltaTime * TOOL_DEPLOY_SPEED;
+        float deployAmount = Time.deltaTime * TOOL_DEPLOY_SPEED;
+
+        switch(toolStatus){
+            case TOOL_RETRACTED:
+            case TOOL_SWITCHING_TO_CLAW:
+            case TOOL_SWITCHING_TO_DRILL:
+                toolOffsetY -= deployAmount;
+                break;
+            case TOOL_DEPLOYED:
+                toolOffsetY += deployAmount;
+                break;
+        }
+
+        if(toolOffsetY <= 0){ // TODO make this tool offset just switch the model?
+            toolOffsetY = 0;
+            switch(toolStatus){
+                case TOOL_SWITCHING_TO_CLAW:
+                    Debug.Log("selecting claw");
+                    tool = TOOL_CLAW;
+                    toolStatus = TOOL_RETRACTED;
+                    break;
+                case TOOL_SWITCHING_TO_DRILL:
+                    Debug.Log("selecting drill");
+                    tool = TOOL_DRILL;
+                    toolStatus = TOOL_RETRACTED;
+                    break;
+                }
+        } else if(toolOffsetY > TOOL_OFFSET_MAX){
+            toolOffsetY = TOOL_OFFSET_MAX;
+        }
+
+        Vector3 newToolPosition = new Vector3(0, -toolOffsetY, 0);
 
         if(TOOL_DRILL == tool){
-            switch(toolStatus){
-                case TOOL_RETRACTED:
-                    drillOffsetY = - increaseAmount;
-                    break;
-                case TOOL_DEPLOYED:
-                    drillOffsetY = + increaseAmount;
-                    break;
-            }
+            drill.localPosition = newToolPosition;
         } else if(TOOL_CLAW == tool){
-            // TODO later
+            claw.localPosition = newToolPosition;
         }
-
-        if(drillOffsetY < 0){
-            drillOffsetY = 0;
-        } else if(drillOffsetY > MAX_OFFSET_Y_DRILL){
-            drillOffsetY = MAX_OFFSET_Y_DRILL;
-        }
-
-        // drill.transform.position = new Vector3(drillInitialOffset.x, drillInitialOffset.y - drillOffsetY, drillInitialOffset.z);
-        // float totalDrillOffset = drillInitialOffsetY + drillOffsetY;
-        Vector3 newDrillPosition = drill.transform.position;
-        float newY = drillInitialOffsetY - drillOffsetY;
-        if(newY > maxFuckingOffsetY){
-            newY = maxFuckingOffsetY;
-        } else if(newY < minFuckingOffsetY){
-            newY = minFuckingOffsetY;
-        }
-        newDrillPosition.y = newY;
-
-
-        // drill.transform.position = newDrillPosition;
 
 	}
 
-    public const int TOOL_DRILL   = 1;
-    public const int TOOL_CLAW    = 2;
-    public const float MAX_OFFSET_Y_DRILL = 0.7f;
-    public const float MAX_OFFSET_Y_CLAW = 1f;
-
-    public const int TOOL_DEPLOYED    = 11;
-    public const int TOOL_RETRACTED   = 12;
-    public const float TOOL_DEPLOY_SPEED = 4f;
+    public const int TOOL_DRILL                 = 1;
+    public const int TOOL_CLAW                  = 2;
+    public const int TOOL_DEPLOYED              = 11;
+    public const int TOOL_RETRACTED             = 12;
+    public const int TOOL_SWITCHING_TO_CLAW     = 13;
+    public const int TOOL_SWITCHING_TO_DRILL    = 14;
+    public const float TOOL_OFFSET_MAX          = 0.85f;
+    public const float TOOL_DEPLOY_SPEED        = 1.2f;
 
 }
